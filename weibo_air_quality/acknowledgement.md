8a877b6dacf525041350f47543f73c3e8be6498f
Ruoqi Zhang <ruoqizhang@college.harvard.edu>
2019-04-25 02:10:30 -0400

    Fleshing out the application more and adding more details. Finalizes sectioning

---
+++ weibo_air_quality/acknowledgement.md
@@ -0,0 +1,41 @@
+This project makes use of datasets generously released by researchers and developers of R packages, without which this Shiny application, in itself a testament of the vibrancy of R developer community, would not be possible. I would like to extend my thanks to the following scholars, researchers, and teams whose datasets form an essential part of this application.
+
+### Weiboscope Open Data at Hong Kong University
+
+King-wa Fu, CH Chan, and Michael Chau's amazing Weiboscope Open Dataset provides an interesting glimpse into the dynamic of Chinese Internet and civil society at that time prior to intensified repressions in recent years. With millions rows of records totaling over 45 Gigabyte, he Weiboscope Open Dataset, which encompasses Weibo posts and their matadata in 2012 for Weibo users with a subscriber base larger than 1,000 people, is unparalleled in its detailed recording of information and its special attention on the issue of censorship. 
+
+To access their dataset, please visit https://hub.hku.hk/cris/dataset/dataset107483.
+
+**Usage in Project**
+
+**Citation**
+
+King-wa Fu, CH Chan, and Michael Chau. Assessing Censorship on Microblogs in China: Discriminatory Keyword Analysis and Impact Evaluation of the 'Real Name Registration' Policy. IEEE Internet Computing. 2013; 17(3): 42-50. http://doi.ieeecomputersociety.org/10.1109/MIC.2013.28
+
+### U.S. Mission to China Air Quality Data
+
+**U.S. Mission to China Air Quality Historical Data**
+U.S. Department of State. 2019. U.S. Department of State Air Quality Monitoring Program. http://www.stateair.net/web/historical/1/1.html
+
+### NASA Socioeconomic Data and Applications Center (SEDAC)
+
+NASA's Moderate Resolution Imaging Spectroradiometer (MODIS), Multi-angle Imaging SpectroRadiometer (MISR), and the Sea-Viewing Wide Field-of-View Sensor (SeaWiFS) observations are used by the researchers to estimate the concentration of fine particulae matters that is smaller than 2.5 milimeter. Such satellite data is not subject to human manipulation and is generally more accurate. 
+
+This project utilizes Global Annual PM2.5 Grids from MODIS, MISR and SeaWiFS Aerosol Optical Depth (AOD) with GWR, v1 (1998–2016) derived from the previous observations. Ranging from 1998 to 2016, the data, provided in GeoTiff format, present a coherent outlook into the trend of air pollution in China.
+
+**Usage in Project**
+
+* A Troubling Trend: Air Pollution in China Across the Years.
+
+**Citation**
+
+van Donkelaar, A., R. V. Martin, M. Brauer, N. C. Hsu, R. A. Kahn, R. C. Levy, A. Lyapustin, A. M. Sayer, and D. M. Winker. 2016. Global Estimates of Fine Particulate Matter Using a Combined Geophysical-Statistical Method with Information from Satellites. Environmental Science & Technology 50 (7): 3762-3772. https://doi.org/10.1021/acs.est.5b05833.
+
+### Global Administrative Areas Dataset
+
+This project makes use of Global Administrative Areas (GADM) Dataset to assist in plotting out the boundaries between administrative entities and to categorize the longitude-latitude Weibo geo-location data into specific administrative areas. GADM dataset represents countries' actural control over territory. **The territory presented in the GADM dataset does not reflect this project's view on countries' sovereignty claims or territorial disputes.**
+
+**Citation**
+
+Global Administrative Areas (2018). GADM database of Global Administrative Areas, version 3.6. [online] URL: www.gadm.org.
+
--- weibo_air_quality/ui.R
+++ weibo_air_quality/ui.R
@@ -34,6 +34,7 @@ shinyUI(fluidPage(
         column(width = 3),
         column(width = 6,
                tags$br(),
+               tags$h2("Pollution: A Legitimacy Issue"),
 
                tags$p(tags$span("T",class = "firstcharacter"), "hree hundred million. That is the number of Chinese people who had watched ", tags$i("Under the Done"),", a documentary on air pollution produced by a former investigative journalist. Despite the documentary, viewed by more than a quarter of China’s population, had been erased of all its traces on the Internet seven days after its release, it had seared one word into the collective consciousness of the Chinese populace."),
                tags$p(tags$b('"Smog."')),
@@ -44,10 +45,13 @@ shinyUI(fluidPage(
       ),
       
       fluidRow(
-        column(width = 3,
+        column(width = 1),
+        sidebarPanel(width = 3,
+               tags$h4("A Troubling Trend: Air Pollution in China Across the Years"),
+               helpText("Drag to view the air pollution (PM2.5) map in a specific year."),
                sliderInput(
                  inputId = "pm25_year",
-                 label = "year",
+                 label = "Year of Map",
                  min = 1998,
                  max = 2016,
                  step = 1,
@@ -55,9 +59,15 @@ shinyUI(fluidPage(
                  animate = TRUE,
                  ticks = FALSE,
                  sep = ""
-               )),
-        column(width = 9,
-               imageOutput("pm25map"))
+               )
+               ),
+        column(width = 7,
+               imageOutput("pm25map"),
+               tags$p(class = "source-note", "Pollution Data Source: van Donkelaar, A., R. V. Martin, M. Brauer, N. C. Hsu, R. A. Kahn, R. C. Levy, A. Lyapustin, A. M. Sayer, and D. M. Winker. 2016. Global Estimates of Fine Particulate Matter Using a Combined Geophysical-Statistical Method with Information from Satellites. Environmental Science & Technology 50 (7): 3762-3772. https://doi.org/10.1021/acs.est.5b05833."),
+               tags$p(class = "source-note", "Map Data Source: Global Administrative Areas (2018). GADM database of Global Administrative Areas, version 3.6. [online] URL: www.gadm.org. Map represents de facto territory. This map does not reflect this project's view on countries' sovereignty claims or territorial disputes.")
+        ),
+        
+        column(width = 1)
       ),
       
       fluidRow(
@@ -71,18 +81,13 @@ shinyUI(fluidPage(
         column(width = 3)
       ),
       
-      # fluidRow(
-      #   column(width = 2),
-      #   column(width = 8,
-      #          imageOutput("censorshipTimeDist")),
-      #   column(width = 2)
-      # ),
-      
       
       sidebarLayout(
+        position = "right",
         sidebarPanel(
+          helpText("Select the range of date in 2012 for displayed Weibo posts statistics."),
           dateRangeInput(inputId = "creationTimeDate",
-                         label = "Date",
+                         label = "Range of Date",
                          start = "2012-01-01",
                          end = "2012-12-31",
                          min = "2012-01-01",
@@ -92,35 +97,80 @@ shinyUI(fluidPage(
         
         # Show a plot of the generated distribution
         mainPanel(
-          plotOutput("creationTimePlot")
+          plotOutput("creationTimePlot"),
+          tags$p(class = "source-note", "Data Source: King-wa Fu, CH Chan, Michael Chau. Assessing Censorship on Microblogs in China: Discriminatory Keyword Analysis and Impact Evaluation of the 'Real Name Registration' Policy. IEEE Internet Computing. 2013; 17(3): 42-50. http://doi.ieeecomputersociety.org/10.1109/MIC.2013.28")
         )
+      ),
+      
+      fluidRow(
+        column(width = 3),
+        column(width = 6,
+               tags$br(),
+               tags$p(tags$span("I",class = "firstcharacter"), "n this project, we return to 2012, and examine how online discussions in Weibo engages with environmental damages and air pollutions. Through geographical locator associated with Weibo posts and historical air quality data, we delve into how air quality in a city affects the sentiment expressed by its dwellers on social media. We would then cut into the issue of censorship, and examine the practice of cencorship's trend in this period."),
+               tags$br()
+               ),
+        column(width = 3)
       )
     ),
     
     tabPanel(
-      title = "Censorship"
-    ),
-    
-    tabPanel(
-      
-      title = "About",
+      title = "Woes of Development",
       fluidRow(
-        # includeCSS("www/header.css"),
-        # tags$div(class="landing-wrapper",
-        #          tags$img(src="https://live.staticflickr.com/7508/15769708856_3ecc0c9c43_k.jpg"),
-        #          tags$div(
-        #            class="caption",
-        #            tags$span(class="border", style="font-size: 45px; font-family: 'Lora', serif;", "SETTLING THE DUST"),
-        #            tags$br(),
-        #            tags$br(),
-        #            tags$span(class="border", "Air Pollution & Public Opinion in China")
-        #          ),
-                includeMarkdown("about.md")
-        )
+        column(width = 3),
+        column(width = 6,
+               tags$h2("Woes of Development")
+        ),
+        column(width = 3)
+      )
+      
     ),
     
     tabPanel(
-      title = "Acknowledgement"
+      title = "Bad Air, Bad Day?",
+      fluidRow(
+        column(width = 3),
+        column(width = 6,
+               tags$h2("Bad Air, Bad Day?")
+        ),
+        column(width = 3)
+      )
+      
+    ),
+    
+    
+    tabPanel(
+      title = '"Permission Denied"',
+      
+      fluidRow(
+        column(width = 3),
+        column(width = 6,
+               tags$h2('"Permission Denied"')
+        ),
+        column(width = 3)
+      ),
+      
+      
+      fluidRow(
+        column(width = 2),
+        column(width = 8,
+               imageOutput("censorshipTimeDist"),
+               tags$p(class = "source-note", "Data Source: King-wa Fu, CH Chan, Michael Chau. Assessing Censorship on Microblogs in China: Discriminatory Keyword Analysis and Impact Evaluation of the 'Real Name Registration' Policy. IEEE Internet Computing. 2013; 17(3): 42-50. http://doi.ieeecomputersociety.org/10.1109/MIC.2013.28")
+        ),
+        column(width = 2)
+      )
+      
+    ),
+    
+    tabPanel(
+      title = "About",
+      fluidPage(
+        column(width = 3),
+        column(width = 6,
+               includeMarkdown("acknowledgement.md"),
+               includeMarkdown("about.md")
+        ),
+        column(width = 3)
+      )
     )
   )
 ))
