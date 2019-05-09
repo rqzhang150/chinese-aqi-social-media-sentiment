e25fec332534d0e41b43f59692aa684d85c41759
Ruoqi Zhang <ruoqizhang@college.harvard.edu>
2019-05-09 14:23:42 -0400

    Preliminary air quality data analysis

--- sentiment_helper.R
+++ sentiment_helper.R
@@ -39,7 +39,62 @@ beijing_sentiment <- beijing %>%
 
 write_rds(beijing_sentiment, "sentiment_analysis/beijing_sentiment.rds")
 
+# ------
+
+guangzhou_aqi <- read_csv("datasets/us_mission_air_quality/Guangzhou_2012_HourlyPM25_created20140423.csv",
+                          skip = 2,
+                          col_types = cols(
+                            Site = col_character(),
+                            Parameter = col_character(),
+                            `Date (LST)` = col_datetime(format = ""),
+                            Year = col_double(),
+                            Month = col_double(),
+                            Day = col_double(),
+                            Hour = col_double(),
+                            Value = col_double(),
+                            Unit = col_character(),
+                            Duration = col_character(),
+                            `QC Name` = col_character()
+                            )
+                          ) %>% 
+  clean_names() %>% 
+  filter(qc_name == "Valid") %>% 
+  mutate(date = as_date(date_lst)) %>% 
+  group_by(date) %>% 
+  summarize(avg_aqi = mean(value))
+
+beijing_aqi <- read_csv("datasets/us_mission_air_quality/Beijing_2012_HourlyPM2.5_created20140325.csv",
+                          skip = 2,
+                          col_types = cols(
+                            Site = col_character(),
+                            Parameter = col_character(),
+                            `Date (LST)` = col_datetime(format = ""),
+                            Year = col_double(),
+                            Month = col_double(),
+                            Day = col_double(),
+                            Hour = col_double(),
+                            Value = col_double(),
+                            Unit = col_character(),
+                            Duration = col_character(),
+                            `QC Name` = col_character()
+                          )
+) %>% 
+  clean_names() %>% 
+  filter(qc_name == "Valid") %>% 
+  mutate(date = as_date(date_lst)) %>% 
+  group_by(date) %>% 
+  summarize(avg_aqi = mean(value))
+
+
+beijing_aqi %>% 
+  ggplot(aes(x = date, y = avg_aqi)) +
+    geom_line()
+
+# ------
+
 shenzhen_sentiment <- read_rds("sentiment_analysis/shenzhen_sentiment.rds")
+guangzhou_sentiment <- read_rds("sentiment_analysis/guangzhou_sentiment.rds")
+
 
 guangzhou_sentiment %>% 
   filter(!is.na(sentiment)) %>% 
@@ -47,7 +102,12 @@ guangzhou_sentiment %>%
   group_by(created_date) %>% 
   summarize(avg_sentiment = mean(sentiment)) %>% 
   ggplot(aes(x = created_date, y = avg_sentiment)) +
-  geom_line() 
+  geom_line() +
+  scale_y_continuous(name = "Sentiment Score",
+                     limits = c(-1, 1),
+                     breaks = seq(-1, 1, by = 0.5)) +
+  theme_minimal()
+  
 
 shenzhen_plot <- shenzhen_sentiment %>% 
   filter(!is.na(sentiment)) %>% 
